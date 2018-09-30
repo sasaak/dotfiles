7d58448f3919df429ebafbedb8579aa245fae864
Mister Umeko <43111030+misterumeko@users.noreply.github.com>
2018-09-30 15:58:24 +0900

    Add Boxstarter script

---
+++ scripts/boxstarter.ps1
@@ -0,0 +1,75 @@
+# Install Boxstarter:
+# . { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
+
+# Run (in Boxstarter Shell):
+# Install-BoxstarterPackage -PackageName <THIS_URL> -DisableReboots
+
+Disable-UAC
+
+Disable-BingSearch
+
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+
+# Provisioned Windows apps (https://docs.microsoft.com/en-us/windows/application-management/apps-in-windows-10#provisioned-windows-apps)
+Get-AppxPackage Microsoft.3DBuilder | Remove-AppxPackage
+Get-AppxPackage Microsoft.DesktopAppInstaller | Remove-AppxPackage
+Get-AppxPackage Microsoft.WindowsFeedbackHub | Remove-AppxPackage
+Get-AppxPackage Microsoft.GetHelp | Remove-AppxPackage
+Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
+Get-AppxPackage Microsoft.ZuneMusic | Remove-AppxPackage
+Get-AppxPackage Microsoft.windowscommunicationsapps | Remove-AppxPackage
+Get-AppxPackage Microsoft.Messaging | Remove-AppxPackage
+Get-AppxPackage Microsoft.People | Remove-AppxPackage
+Get-AppxPackage Microsoft.Windows.Photos | Remove-AppxPackage
+Get-AppxPackage Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
+# Get-AppxPackage Microsoft.MicrosoftStickyNotes | Remove-AppxPackage
+Get-AppxPackage Microsoft.Getstarted | Remove-AppxPackage
+Get-AppxPackage Microsoft.Microsoft3DViewer | Remove-AppxPackage
+Get-AppxPackage Microsoft.ZuneVideo | Remove-AppxPackage
+Get-AppxPackage Microsoft.BingWeather | Remove-AppxPackage
+Get-AppxPackage Microsoft.Office.OneNote | Remove-AppxPackage
+Get-AppxPackage Microsoft.OneConnect | Remove-AppxPackage
+Get-AppxPackage Microsoft.MSPaint | Remove-AppxPackage
+Get-AppxPackage Microsoft.Print3D | Remove-AppxPackage
+Get-AppxPackage Microsoft.SkypeApp | Remove-AppxPackage
+Get-AppxPackage Microsoft.WebMediaExtensions | Remove-AppxPackage
+Get-AppxPackage Microsoft.WindowsAlarms | Remove-AppxPackage
+Get-AppxPackage Microsoft.WindowsCalculator | Remove-AppxPackage
+# Get-AppxPackage Microsoft.WindowsCamera | Remove-AppxPackage
+Get-AppxPackage Microsoft.WindowsMaps | Remove-AppxPackage
+# Get-AppxPackage Microsoft.WindowsStore | Remove-AppxPackage
+# Get-AppxPackage Microsoft.SoundRecorder | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxApp | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxGameOverlay | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxGamingOverlay | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxIdentityProvider | Remove-AppxPackage
+Get-AppxPackage Microsoft.Xbox.TCUI | Remove-AppxPackage
+
+# Installed Windows apps (https://docs.microsoft.com/en-us/windows/application-management/apps-in-windows-10#installed-windows-apps)
+Get-AppxPackage Microsoft.BingNews | Remove-AppxPackage
+
+# Other
+Get-AppxPackage *Dell* | Remove-AppxPackage
+Get-AppxPackage *LinkedIn* | Remove-AppxPackage
+Get-AppxPackage *McAfee* | Remove-AppxPackage
+Get-AppxPackage *Netflix* | Remove-AppxPackage
+Get-AppxPackage *Dropbox* | Remove-AppxPackage
+Get-AppxPackage *CandyCrush* | Remove-AppxPackage
+Get-AppxPackage *MarchofEmpires* | Remove-AppxPackage
+
+choco install Microsoft-Hyper-V-All -source windowsFeatures
+choco install Microsoft-Windows-Subsystem-Linux -source windowsfeatures
+
+choco install vscode
+choco install googlechrome
+choco install firefox
+choco install docker-for-windows
+choco install 7zip.install
+choco install git.install
+choco install sourcetree
+choco install hyper
+choco install keyhac
+
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
