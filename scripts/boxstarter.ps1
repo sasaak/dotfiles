509a0155550c3e1a71b4c3b95c379d2910e1f3b8
sasaak <39662595+sasaak@users.noreply.github.com>
2019-04-14 12:20:18 +0900

    自分用に改造

--- scripts/boxstarter.ps1
+++ scripts/boxstarter.ps1
@@ -7,29 +7,42 @@
 
 Disable-UAC
 Disable-BingSearch
-Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileExtensions
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+Set-TaskbarOptions -Size Small -Dock Bottom -Combine Full -Lock
+Enable-RemoteDesktop
 
+# Code
 choco install Microsoft-Hyper-V-All -source windowsFeatures
 choco install Microsoft-Windows-Subsystem-Linux -source windowsfeatures
-
-choco install vscode
-choco install googlechrome
-choco install firefox
 choco install docker-for-windows
-choco install 7zip.install
 choco install git.install
-# choco install sourcetree # Chocolatey経由だとスタートメニューに表示されない。
-choco install hyper
-choco install keyhac
-choco install adobereader
-# choco install visualstudio2017community # Boxstarter経由だと失敗する (PowerShellだとOK)。
-choco install vlc
+choco install svn
 choco install anaconda3
 choco install nvm
+choco install vscode
+choco install plantuml
+choco install jetbrainstoolbox
+choco install tortoisesvn
+choco install tortoisegit
+choco install hyper
 
+# Tools
+choco install googlejapaneseinput
+choco install 7zip.install
+choco install imgburn
+choco install inkscape
+choco install gimp
+
+# Apps
+choco install googlechrome
+choco install firefox
+choco install adobereader
+choco install mpc-hc
+choco install keepass.install
+
+# Scripts
 nvm install 8.12.0
 nvm use 8.12.0
-
 Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile ~/Ubuntu.appx -UseBasicParsing
 Add-AppxPackage -Path ~/Ubuntu.appx
 
